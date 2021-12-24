3fb21284e8f15f276a5f9cb025f749e1e43357aa
Kemal <ttnckemal@gmail.com>
2021-12-24 14:38:00 +0100

    commt

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const guessNumber = Math.floor(Math.random() * 100) + 1;
 console.log(guessNumber);
 const input = document.querySelector(".input-number");
 const btn = document.querySelector(".btn");
-// let noOfguess = 1;
+let noOfguess = 1;
 
 let sayac = 5
 
@@ -20,17 +20,16 @@ btn.addEventListener("click", () => {
             if (val < guessNumber) {
                 document.querySelector(".input-number").value = ""
                 document.querySelector(".span").innerText = "go up"
-                    // document.querySelector("#message2").innerText = "No Of Guesses : " + noOfguess
-                    // document.querySelector("#message3").innerText = "Guessed Number Are: " + val
+                document.querySelector("#message2").innerText = "No Of Guesses : " + noOfguess
+                document.querySelector("#message3").innerText = "Guessed Number Are: " + val
             } else if (val == guessNumber) {
-
-                document.querySelector(".container").innerHTML = "<h1 style='color: greenyellow;'>You won it!</h1>"
+                document.querySelector(".container").innerHTML = "<h1 style='color: greenyellow;'>You won it!</h1> <br> <p>The number was:</p> " + guessNumber + "<br><br> <a style='color: greenyellow;' href='./index.html'> Try it again</a>"
 
             } else {
                 document.querySelector(".input-number").value = ""
                 document.querySelector(".span").innerText = " go down"
-                    // document.querySelector("#message2").innerText = "No Of Guesses : " + noOfguess
-                    // document.querySelector("#message3").innerText = "Guessed Number Are: " + val
+                document.querySelector("#message2").innerText = "No Of Guesses : " + noOfguess
+                document.querySelector("#message3").innerText = "Guessed Number Are: " + val
             }
             document.querySelector("#message1").innerText = sayac
         } else {
